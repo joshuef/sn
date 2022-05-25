b132d2ed66fff4e9b66dcc5e1193e2c2e8a8cb3d
Josh Wilson <joshuef@gmail.com>
2022-05-25 18:38:42 +0200

    initial data batching for priority

--- sn_node/src/node/core/messaging/handling/update_section.rs
+++ sn_node/src/node/core/messaging/handling/update_section.rs
@@ -18,49 +18,74 @@ use sn_interface::{
     types::ReplicatedDataAddress,
 };
 use std::collections::BTreeSet;
+use xor_name::XorName;
 
 impl Node {
-
     /// Gets data we hold, that is relevant to a specific sender, it returns a Vec of Vecs,
     /// each inner vec holding ReplicatedDatAddresses of lessening proximity to the holder
     /// This assumes that the requester has asked us a valid question...
-    async fn get_data_for_holder( &self, sender: Peer ) -> Vec<Vec<ReplicatedDataAddress>>> {
-        // TODO: can we cache this data stored per churn event?
-        let data_i_have = self.data_storage.keys().await?;
-        trace!("Our data got");
-
+    async fn get_data_for_holder(
+        &self,
+        data_i_have: Vec<ReplicatedDataAddress>,
+        sender: Peer,
+    ) -> Result<Vec<Vec<ReplicatedDataAddress>>> {
         // You sort by xorname for all data keys
         // you take adults known / replica count's worth of names.
         // those are then prioritised into batches...
-
 
         // let known_adults = self.network_knowledge.adults().await.len();
         let data_copies = data_copy_count();
 
         // how much of the held data should any one node be responsible for
-        let data_per_holder = data_i_have.len().checked_div(data_copies) ;
+        let data_per_holder = data_i_have.len().checked_div(data_copies);
         // if we're responsible for any of it...
-        if let Some ( data_per_holder ) = data_per_holder {
+        if let Some(data_per_holder) = data_per_holder {
             debug!("{sender} is responsible for {data_per_holder} of data");
 
             // let data_prio_1: Vec<_> =
 
+            let sorted_data = data_i_have
+                .into_iter()
+                .sorted_by(|lhs, rhs| sender.name().cmp_distance(lhs.name(), rhs.name()))
+                .collect::<Vec<_>>();
 
-            data_i_have
-                .sorted_by(|lhs, rhs| sender.name().cmp_distance(lhs, rhs))
-                .collect()
+            let data_chunks = sorted_data
                 .chunks(data_per_holder)
+                .map(|a| {
+                    a.iter().cloned().collect_vec()
+                })
+                // .collect::<Vec<_>>();
+                // .collect_vec()
+                .collect_vec();
 
+            Ok(data_chunks)
             // debug!("len of direct data responsibility: {:?}", data_prio_1.len());
-
+        } else {
+            Ok(vec![])
         }
-        else {
-            vec![]
-        }
-
-
     }
 
+    /// Given a peer and a `greed level`, we check if a peer should be holding certain data.
+    /// A greed_level of 0 will result in true being returned only if the peer is within
+    /// `data_copy_count` nodes of the data.
+    async fn check_if_node_should_hold_data_in_section(
+        &self,
+        node_name: &XorName,
+        data: &ReplicatedDataAddress,
+        greed_level: usize,
+    ) -> bool {
+        let adults = self.network_knowledge.adults().await;
+        let adults_names = adults.iter().map(|p2p_node| p2p_node.name());
+
+        let holder_breadth = data_copy_count() + greed_level;
+        let holder_adult_list: BTreeSet<_> = adults_names
+            .clone()
+            .sorted_by(|lhs, rhs| data.name().cmp_distance(lhs, rhs))
+            .take(holder_breadth)
+            .collect();
+
+        holder_adult_list.contains(node_name)
+    }
 
     /// Given a set of known data, we can calculate what more from what we have a
     /// given node should be responsible for
@@ -72,52 +97,58 @@ impl Node {
     ) -> Result<Vec<Cmd>> {
         trace!("Getting missing data for node");
         // Collection of data addresses that we do not have
+        // TODO: can we cache this data stored per churn event?
+        let data_i_have = self.data_storage.keys().await?;
+        trace!("Our data got");
 
-        let data_i_have = get_data_for_holder(sender);
+        let data_chunked_per_holder_quantity =
+            self.get_data_for_holder(data_i_have, sender).await?;
 
-        if data_i_have.is_empty() {
+        if data_chunked_per_holder_quantity.is_empty() {
             trace!("We have no data for sender");
             return Ok(vec![]);
         }
 
-        let adults = self.network_knowledge.adults().await;
-        let adults_names = adults.iter().map(|p2p_node| p2p_node.name());
+        let mut cmds = vec![];
+        // TODO: parallelise
+        for (i, dataset) in data_chunked_per_holder_quantity.into_iter().enumerate() {
+            let mut data_for_sender = vec![];
 
-        let mut data_for_sender = vec![];
-        for data in data_i_have {
-            if data_sender_has.contains(&data) {
+            for data in dataset {
+                if data_sender_has.contains(&data) {
+                    continue;
+                }
+
+                // Right now we just want data_copy but, this could be increased easily via
+                // sender node eg.
+                let greed_level = 0;
+                let should_hold_data = self
+                    .check_if_node_should_hold_data_in_section(&sender.name(), &data, 0)
+                    .await;
+
+                if should_hold_data {
+                    debug!("Our requester should hold: {:?}", data);
+                    let _existed = data_for_sender.push(data.clone());
+                }
+            }
+
+            if data_for_sender.is_empty() {
+                trace!("We have no data worth sending");
                 continue;
             }
+            debug!(
+                "{:?} batch to: {:?} ",
+                LogMarker::QueuingMissingReplicatedData,
+                sender
+            );
 
-            let holder_adult_list: BTreeSet<_> = adults_names
-                .clone()
-                .sorted_by(|lhs, rhs| data.name().cmp_distance(lhs, rhs))
-                .take(data_copy_count())
-                .collect();
-
-            if holder_adult_list.contains(&sender.name()) {
-                debug!("Our requester should hold: {:?}", data);
-                let _existed = data_for_sender.push(data);
-            }
+            cmds.push(Cmd::EnqueueDataForReplication {
+                recipient: sender,
+                data_batch: data_for_sender,
+            });
         }
 
-        if data_for_sender.is_empty() {
-            trace!("We have no data worth sending");
-            return Ok(vec![]);
-        }
-
-        debug!(
-            "{:?} batch to: {:?} ",
-            LogMarker::QueuingMissingReplicatedData,
-            sender
-        );
-
-        let cmd = Cmd::EnqueueDataForReplication {
-            recipient: sender,
-            data_batch: data_for_sender,
-        };
-
-        Ok(vec![cmd])
+        Ok(cmds)
     }
 
     /// Will send a list of currently known/owned data to relevant nodes.
