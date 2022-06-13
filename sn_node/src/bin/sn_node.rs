f2d01d13974473780f73d228e4fb955ecd0cdbbd
Josh Wilson <joshuef@gmail.com>
2022-06-13 10:52:36 +0200

    feat: change to single threaded runtime by default

--- sn_node/src/bin/sn_node.rs
+++ sn_node/src/bin/sn_node.rs
@@ -55,7 +55,7 @@ use tracing_subscriber::filter::EnvFilter;
 
 #[cfg(not(feature = "tokio-console"))]
 const MODULE_NAME: &str = "sn_node";
-const BOOTSTRAP_RETRY_TIME_SEC: u64 = 30;
+const BOOTSTRAP_RETRY_TIME_SEC: u64 = 15;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -109,7 +109,12 @@ fn create_runtime_and_node() -> Result<()> {
     info!("Node runtime started");
 
     // start a new runtime for a node.
-    let rt = tokio::runtime::Runtime::new()?;
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .thread_name("sn_node")
+        .thread_stack_size(16 * 1024 * 1024)
+        .build()?;
+
     let _res = rt.block_on(async move {
         // pull config again in case it has been updated meanwhile
         let config = Config::new().await?;
