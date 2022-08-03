1374c0d717b11e8df90c800644ef088dc9f68310
Josh Wilson <joshuef@gmail.com>
2022-08-03 16:48:53 +0200

    chore: cleanup unused code

--- sn_node/src/node/messages.rs
+++ sn_node/src/node/messages.rs
@@ -9,27 +9,14 @@
 use crate::node::{Error, Result};
 
 use sn_interface::{
-    messaging::{
-        system::{SigShare, SystemMsg},
-        AuthKind, AuthorityProof, BlsShareAuth, Dst, MsgId, NodeAuth, WireMsg,
-    },
-    network_knowledge::{NodeInfo, SectionKeyShare},
+    messaging::{system::SystemMsg, AuthKind, Dst, MsgId, NodeAuth, WireMsg},
+    network_knowledge::NodeInfo,
 };
 
 use bls::PublicKey as BlsPublicKey;
-use xor_name::XorName;
 
 // Utilities for WireMsg.
 pub(crate) trait WireMsgUtils {
-    /// Creates a message signed using a BLS `KeyShare` for destination accumulation
-    fn for_dst_accumulation(
-        key_share: &SectionKeyShare,
-        src_name: XorName,
-        dst: Dst,
-        node_msg: SystemMsg,
-        src_section_pk: BlsPublicKey,
-    ) -> Result<WireMsg, Error>;
-
     /// Creates a signed message from single node.
     fn single_src(
         node: &NodeInfo,
@@ -40,29 +27,6 @@ pub(crate) trait WireMsgUtils {
 }
 
 impl WireMsgUtils for WireMsg {
-    /// Creates a message signed using a BLS `KeyShare` for destination accumulation
-    fn for_dst_accumulation(
-        key_share: &SectionKeyShare,
-        src_name: XorName,
-        dst: Dst,
-        msg: SystemMsg,
-        src_section_pk: BlsPublicKey,
-    ) -> Result<WireMsg, Error> {
-        let msg_payload =
-            WireMsg::serialize_msg_payload(&msg).map_err(|_| Error::InvalidMessage)?;
-
-        let auth = AuthKind::NodeBlsShare(
-            bls_share_authorize(src_section_pk, src_name, key_share, &msg_payload).into_inner(),
-        );
-
-        let wire_msg = WireMsg::new_msg(MsgId::new(), msg_payload, auth, dst);
-
-        #[cfg(feature = "test-utils")]
-        let wire_msg = wire_msg.set_payload_debug(msg);
-
-        Ok(wire_msg)
-    }
-
     /// Creates a signed message from single node.
     fn single_src(
         node: &NodeInfo,
@@ -84,22 +48,4 @@ impl WireMsgUtils for WireMsg {
 
         Ok(wire_msg)
     }
-}
-
-// Construct verified authority of a single node's share of section authority.
-fn bls_share_authorize(
-    section_pk: BlsPublicKey,
-    src_name: XorName,
-    key_share: &SectionKeyShare,
-    payload: impl AsRef<[u8]>,
-) -> AuthorityProof<BlsShareAuth> {
-    AuthorityProof(BlsShareAuth {
-        section_pk,
-        src_name,
-        sig_share: SigShare {
-            public_key_set: key_share.public_key_set.clone(),
-            index: key_share.index,
-            signature_share: key_share.secret_key_share.sign(payload),
-        },
-    })
 }
