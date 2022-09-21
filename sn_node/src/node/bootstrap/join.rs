maidsafe/temp_safe_network

chore: dont try and establish connections for ServiceMsgs

sn_node/src/node/bootstrap/join.rs
@@ -503,7 +503,7 @@ async fn send_messages(
             let msg_id = msg.msg_id();
 
             let bytes = msg.serialize()?;
-            match comm.send(peer, msg_id, bytes).await {
+            match comm.send(peer, msg_id, bytes, false).await {
                 Ok(()) => trace!("Msg {msg_id:?} sent on {dst:?}"),
                 Err(Error::FailedSend(peer)) => {
                     error!("Failed to send message {msg_id:?} to {peer:?}")

joshuef/sn

sn_node/src/node/bootstrap/join.rs
@@ -497,7 +497,7 @@ async fn send_messages(
             let msg_id = msg.msg_id();
 
             let bytes = msg.serialize()?;
-            match comm.send(peer, msg_id, bytes).await {
+            match comm.send_out_bytes(peer, msg_id, bytes, false).await {
                 Ok(()) => trace!("Msg {msg_id:?} sent on {dst:?}"),
                 Err(Error::FailedSend(peer)) => {
                     error!("Failed to send message {msg_id:?} to {peer:?}")