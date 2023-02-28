8fbdaf66c39989ac3948806c9e89a57feb05e8c1
Josh Wilson <joshuef@gmail.com>
2023-02-28 12:17:32 +0100

    fast fast relocate

--- sn_node/src/node/flow_ctrl/periodic_checks.rs
+++ sn_node/src/node/flow_ctrl/periodic_checks.rs
@@ -21,8 +21,8 @@ use std::{collections::BTreeSet, time::Duration};
 use tokio::time::Instant;
 
 const PROBE_INTERVAL: Duration = Duration::from_secs(300);
-const REQUEST_TO_RELOCATE_TIMEOUT_SEC: Duration = Duration::from_secs(30);
-const JOIN_AS_RELOCATED_TIMEOUT_SEC: Duration = Duration::from_secs(30);
+const REQUEST_TO_RELOCATE_TIMEOUT_SEC: Duration = Duration::from_secs(1);
+const JOIN_AS_RELOCATED_TIMEOUT_SEC: Duration = Duration::from_secs(1);
 const MISSING_VOTE_INTERVAL: Duration = Duration::from_secs(5);
 const MISSING_DKG_MSG_INTERVAL: Duration = Duration::from_secs(5);
 // const SECTION_PROBE_INTERVAL: Duration = Duration::from_secs(300);
